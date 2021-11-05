ceadf7696482645371122be369db28b5ef2bf544
Dragan Miskic <dm61200@gmail.com>
2021-11-05 16:15:15 +0100

    implemented OcenaNaIspitu class

---
+++ src/main/OcenaNaIspitu.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.util.Date;
+
+public class OcenaNaIspitu {
+	private String studentPolozio;
+	private String predmet;
+	private int ocena;
+	private Date datumPolaganja = new Date();
+	
+	public void setOcena(int a) {
+		if (a >= 6 && a <= 10) {
+			ocena = a;
+		} else {
+			throw new IllegalArgumentException("Broj je van opsega");
+		} 
+	}
+}
